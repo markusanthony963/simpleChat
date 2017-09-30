82f809367c20e45c7dd12e88cc6fb57cb3b1b5e0
iosdev <iosdev@labb509-18.exam.umn.ac.id>
2017-09-30 08:58:26 +0700

    Initial Commit

---
+++ simpleChating/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleChating
+//
+//  Created by iosdev on 9/30/17.
+//  Copyright © 2017 ios3-umn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
